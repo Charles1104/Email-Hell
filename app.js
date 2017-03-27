d3d3a9f78125fd1ebaca65c8d77731c7c958de75
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-03-27 10:20:51 -1000

    comments added

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var emailLog = require ('./data.json');
 var bigArray = [];
 var grouping = {};
 
+// this is a function that finds unique values of an array an return an array with these unique values
 function onlyUnique(value, index, self) {
   return self.indexOf(value) === index;
 }
 
+// this is a function that will join two arrays into one big array with each position of this array being an object with two properties. Each property will be the value of the respective value of the single arrays.
 function addArrayToAnObject (arr1,arr2){
   for (var m = 0; m < arr1.length; m++){
     grouping.email = arr1[m];
@@ -18,12 +20,14 @@ function addArrayToAnObject (arr1,arr2){
   return bigArray;
 }
 
+// this funciton is the main one. It will will take the JSON file located in a folder and will return an array of obkects with unique emails and number of time an email was sent.
 
 function uniqueArray(){
 
   var emails = emailLog.emails;
   var uniqueArray = [];
 
+// will create an array of unique emails
   for ( var i = 0; i < emails.length; i++){
     uniqueArray.push(emails[i].email);
   }
@@ -32,6 +36,7 @@ function uniqueArray(){
   uniqueArray = [];
   var count = 0;
 
+// will create an array of the numbers of emails. This array will be the same length of uniqueEmails and position will correspond to the emails of the uniqueEmails array.
   for ( var j = 0; j < uniqueEmails.length; j++){
     for (var k = 0; k < emails.length; k++){
       if(emails[j].email === emails[k].email){
@@ -42,8 +47,10 @@ function uniqueArray(){
     count = 0;
   }
 
+// this funciton will join the two arrays into a big array with each position correpsonding to an object
   addArrayToAnObject (uniqueEmails, uniqueArray);
 
+// return the new big array
   return bigArray;
 
 }
